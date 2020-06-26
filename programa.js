9e4dc58f0bf37933b0355994d6c3eadcdc987012
Ivanna vargas <65833953+ivannavargas@users.noreply.github.com>
2020-06-25 20:47:33 -0500

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,28 @@
+
+
+function numeroAleatorio(min, max) {
+ 
+    var a;
+   
+  
+
+    //covertir el valor de x a un entero 
+    a=document.getElementById("a").value;
+    
+
+    
+    
+  
+  
+    
+  min=Math.random()*a;
+  min=Math.round(min);
+  console.log(min);
+    
+
+
+
+
+}
+
+
